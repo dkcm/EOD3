82dfe9c77e7912e959806bf15fd7dac828cd9fc5
dkcm <daniel.kuan@gmail.com>
2016-04-14 00:41:47 +0800

    Added MotleyFool and WallStreetJournal.

---
+++ src/main/java/org/ikankechil/eod3/sources/MotleyFool.java
@@ -0,0 +1,199 @@
+/**
+ * MotleyFool.java  v0.2  6 June 2014 01:19:57 PM
+ *
+ * Copyright © 2014-2016 Daniel Kuan.  All rights reserved.
+ */
+package org.ikankechil.eod3.sources;
+
+import static org.ikankechil.eod3.sources.Exchanges.*;
+import static org.ikankechil.util.StringUtility.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+import org.ikankechil.eod3.Frequencies;
+import org.ikankechil.io.TextTransform;
+import org.ikankechil.io.TextTransformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A <code>Source</code> representing Motley Fool.
+ * <p>
+ *
+ * @author Daniel Kuan
+ * @version 0.2
+ */
+public class MotleyFool extends Source {
+
+  private final DateFormat    urlDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+
+  // Date-related URL parameters
+  private static final String START_DATE    = "&START_DATE=";
+  private static final String END_DATE      = "&END_DATE=";
+
+  // Exchange-related constants
+  private static final String US            = "US";
+  private static final String UK            = "UK";
+
+  static final Logger         logger        = LoggerFactory.getLogger(MotleyFool.class);
+
+  public MotleyFool() {
+    super("http://www.motleyfool.idmanagedsolutions.com/stocks/historical_quotes.idms?&BLOCKSIZE=20000&OFFSET=0&SYMBOL_");
+
+    // supported markets (only US and UK)
+    exchanges.put(NYSE, US);
+    exchanges.put(NASDAQ, US);
+    exchanges.put(AMEX, US);
+    exchanges.put(NYSEARCA, US);
+    exchanges.put(LSE, UK);
+//    exchanges.put(TSX, TSX.toString());
+
+    // Motley Fool API
+    // e.g.
+    // HTML http://www.motleyfool.idmanagedsolutions.com/stocks/historical_quotes.idms?SYMBOL_US=NSH&START_DATE=2014-06-01&END_DATE=2014-06-03
+    // HTML http://www.motleyfool.idmanagedsolutions.com/stocks/historical_quotes.idms?SYMBOL_US=NSH&START_DATE=2014-01-29&END_DATE=2015-01-29&CODE_MARKET=US,@US&BLOCKSIZE=200&OFFSET=1
+    // HTML http://www.motleyfool.idmanagedsolutions.com/stocks/historical_quotes.idms?START_DATE=2015-01-02&END_DATE=2016-01-02&CODE_MARKET=US,@US&SYMBOL_US=IBM&BLOCKSIZE=100&OFFSET=1
+    //
+    // Notes:
+    // 1. incoming data is in HTML format
+  }
+
+  @Override
+  void appendSymbolAndExchange(final StringBuilder url,
+                               final String symbol,
+                               final Exchanges exchange) {
+    // e.g. SYMBOL_US=NSH
+    appendExchange(url, exchange);
+    url.append(EQUAL);
+    appendSymbol(url, symbol);
+  }
+
+  @Override
+  void appendStartDate(final StringBuilder url, final Calendar start) {
+    // &START_DATE=2014-06-01
+    url.append(START_DATE).append(urlDateFormat.format(start.getTime()));
+    logger.debug("Start date: {}", url);
+  }
+
+  @Override
+  void appendEndDate(final StringBuilder url, final Calendar end) {
+    // &END_DATE=2014-06-03
+    url.append(END_DATE).append(urlDateFormat.format(end.getTime()));
+    logger.debug("End date: {}", url);
+  }
+
+  @Override
+  void appendDefaultDates(final StringBuilder url,
+                          final Calendar start,
+                          final Calendar end) {
+    // start date defaults to 1 January 1970
+    appendStartDate(url, DEFAULT_START);
+    logger.debug("Default start and end dates appended: {}", url);
+  }
+
+  @Override
+  void appendFrequency(final StringBuilder url, final Frequencies frequency) {
+    // do nothing
+    logger.debug(UNSUPPORTED);
+  }
+
+  @Override
+  public TextTransformer newTransformer(final TextTransform transform) {
+    return new TextTransformer(transform) {
+
+      private static final String DATA_START_TAG = "<div id=\"historical_quotes\">";
+      private static final String DATA_END_TAG   = "</div>";
+      private static final String ROW_START_TAG  = "<tr>";
+      private static final String ROW_END_TAG    = "</tr>";
+      private static final String CELL_END_TAG   = "</td>";
+
+      @Override
+      public List<String> transform(final List<String> lines) {
+        final List<String> newLines = new ArrayList<>(lines.size() >> THREE);
+
+        final Iterator<String> linesIterator = lines.iterator();
+        while (linesIterator.hasNext()) {
+          String line = linesIterator.next();
+
+          // data block
+          if (line.contains(DATA_START_TAG)) {
+            while (linesIterator.hasNext()) {
+              line = linesIterator.next();
+
+              // line
+              if (line.contains(ROW_START_TAG)) {
+                final StringBuilder newLine = new StringBuilder();
+                while (linesIterator.hasNext()) {
+                  line = linesIterator.next();
+
+                  // individual values
+                  final int valueEnd = line.lastIndexOf(CELL_END_TAG);
+                  if (valueEnd > -ONE) {
+                    final int valueStart = ONE + line.lastIndexOf(MORE_THAN, valueEnd);
+                    if ((valueStart > ZERO) && (valueStart < valueEnd)) {
+                      newLine.append(line.substring(valueStart, valueEnd))
+                             .append(COMMA);
+                    }
+                  }
+                  // EOL
+                  else if (line.contains(ROW_END_TAG)) {
+                    if (newLine.length() > ZERO) {
+                      final String transformedLine = transform.transform(newLine.toString());
+                      if (transformedLine.length() > ZERO) {
+                        newLines.add(transformedLine);
+                      }
+                    }
+                    break;
+                  }
+                }
+              }
+              // end of data block
+              else if (line.contains(DATA_END_TAG)) {
+                break;
+              }
+            }
+          }
+        }
+
+        lines.clear();
+        lines.addAll(newLines);
+        logger.info("Transformation complete");
+
+        return lines;
+      }
+
+    };
+  }
+
+  @Override
+  public TextTransform newTransform(final String symbol) {
+    return new TextTransform() {
+
+      private static final String COMMAS_AND_WHITESPACE = "[,\\s]";
+
+      @Override
+      public String transform(final String line) {
+        // concatenate segments
+        final StringBuilder builder = new StringBuilder(symbol).append(COMMA);
+        builder.append(line.substring(SIX, TEN))          // year
+               .append(line.substring(ZERO, TWO))         // month
+               .append(line.substring(THREE, FIVE));      // date
+
+        final int volumeStart = ONE + findNth(COMMA, line, FIVE, TEN);
+        builder.append(line.substring(TEN, volumeStart)); // OHLC
+        builder.append(line.substring(volumeStart)        // volume
+                           .replaceAll(COMMAS_AND_WHITESPACE, EMPTY));
+
+        return builder.toString();
+      }
+
+    };
+  }
+
+}
---
+++ src/main/java/org/ikankechil/eod3/sources/WallStreetJournal.java
@@ -0,0 +1,280 @@
+/**
+ * WallStreetJournal.java v0.3  14 May 2014 11:49:20 PM
+ *
+ * Copyright © 2014-2016 Daniel Kuan.  All rights reserved.
+ */
+package org.ikankechil.eod3.sources;
+
+import static org.ikankechil.eod3.sources.Exchanges.*;
+import static org.ikankechil.eod3.sources.WallStreetJournal.Elements.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import org.ikankechil.eod3.Frequencies;
+import org.ikankechil.io.TextReader;
+import org.ikankechil.io.TextTransform;
+import org.ikankechil.io.TextTransformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A <code>Source</code> representing the Wall Street Journal.
+ *
+ * @author Daniel Kuan
+ * @version 0.3
+ */
+class WallStreetJournal extends Source {
+  // TODO Enhancements
+  // 1. v1.0 migrate from dylan to ajax API
+
+  public static void main(final String... args) throws IOException {
+    final URL url = new URL("http://quotes.wsj.com/ajax/historicalprices/7/GLE?exchange=XPAR&country=FR&startDate=05/01/2005&endDate=12/31/2015");
+    final List<String> lines = new TextReader().read(url);
+    for (final String line : lines) {
+      logger.info(line);
+    }
+  }
+
+  private final DateFormat    urlDateFormat = new SimpleDateFormat("MM-dd-yyyy", Locale.US);
+
+  // Date-related URL parameters
+  private static final String START_DATE    = "&startDate=";
+  private static final String END_DATE      = "&endDate=";
+  private static final String FREQUENCY     = "&duration=P1";
+
+  // Country code
+  private static final String COUNTRY_CODE  = "&countrycode=";
+
+  // Exchange-related constants
+  private static final String US            = "US";
+  private static final String UK            = "UK";
+  private static final String DE            = "DE";
+  private static final String FR            = "FR";
+  private static final String SG            = "SG";
+  private static final String HK            = "HK";
+  private static final String JP            = "JP";
+  private static final String IN            = "IN";
+  private static final String AU            = "AU";
+  private static final String CA            = "CA";
+  private static final String TW            = "TW";
+  private static final String IT            = "IT";
+  private static final String PL            = "PL";
+  private static final String NO            = "NO";
+  private static final String SE            = "SE";
+  private static final String DK            = "DK";
+
+  static final Logger         logger        = LoggerFactory.getLogger(WallStreetJournal.class);
+
+  public WallStreetJournal() {
+    super("http://quotes.wsj.com/cdssvco/marketwatch/dylan/v1/HistoricalPrices?pageSize=0&sortColumn=Time&sortOrder=DESC&ticker=");
+
+    // supported markets
+    exchanges.put(NYSE, US);
+    exchanges.put(NASDAQ, US);
+    exchanges.put(AMEX, US);
+    exchanges.put(NYSEARCA, US);
+    exchanges.put(LSE, UK);
+    exchanges.put(FWB, DE);
+    exchanges.put(PAR, FR);
+    exchanges.put(MIB, IT);
+    exchanges.put(SGX, SG);
+    exchanges.put(HKSE, HK);
+    exchanges.put(TSE, JP);
+    exchanges.put(NSE, IN);
+    exchanges.put(TWSE, TW);
+    exchanges.put(ASX, AU);
+    exchanges.put(TSX, CA);
+    exchanges.put(GPW, PL);
+    exchanges.put(OSLO, NO);
+    exchanges.put(SB, SE);
+    exchanges.put(KFB, DK);
+    exchanges.put(FX, US);
+
+    // Wall Street Journal API
+    // http://quotes.wsj.com/ajax/historicalprices/7/<Symbol>?instrumentType=STOCK
+    //                                                       &num_rows=1000
+    //                                                       &country=<Country Code>
+    //                                                       &exchange=<Exchange Code>
+    //                                                       &startDate=<MM/dd/yyyy>
+    //                                                       &endDate=<MM/dd/yyyy>
+    //
+    // http://quotes.wsj.com/cdssvco/marketwatch/dylan/v1/HistoricalPrices?ticker=A
+    //                                                                    &countrycode=US
+    //                                                                    &startDate=01-01-2000
+    //                                                                    &endDate=05-14-2014
+    //                                                                    &duration=P1D
+    //                                                                    &pagePos=0
+    //                                                                    &pageSize=0
+    //                                                                    &sortColumn=Time
+    //                                                                    &sortOrder=DESC
+    //                                                                    &contentType=xls
+    //
+    // e.g.
+    // http://quotes.wsj.com/ajax/historicalprices/7/GLE?country=FR&exchange=XPAR&instrumentType=STOCK&num_rows=1900&startDate=01/21/2013&endDate=12/31/2014
+    // http://quotes.wsj.com/ajax/historicalprices/7/TSLA?MOD_VIEW=page&ticker=TSLA&country=US&exchange=XNAS&instrumentType=STOCK&num_rows=244&range_days=244&startDate=05%2F01%2F2014&endDate=12%2F31%2F2014&_=1451036242782
+    // http://quotes.wsj.com/ajax/historicalprices/7/GLE?MOD_VIEW=page&ticker=GLE&country=FR&exchange=XPAR&instrumentType=STOCK&num_rows=90&range_days=90&startDate=09%2F26%2F2015&endDate=12%2F25%2F2015&_=1451058706241
+    //
+    // Excel:
+    // http://quotes.wsj.com/cdssvco/marketwatch/dylan/v1/HistoricalPrices?ticker=A&countrycode=US&startDate=01-01-2000&endDate=05-14-2014&duration=P1D&pagePos=0&pageSize=0&sortColumn=Time&sortOrder=DESC&contentType=xls
+    //
+    // XML:
+    // http://quotes.wsj.com/cdssvco/marketwatch/dylan/v1/HistoricalPrices?ticker=JCP&countrycode=US&startDate=01-18-1970&endDate=05-15-2014&duration=P1D&pageSize=0&sortColumn=Time&sortOrder=DESC
+    //
+    // Notes:
+    // 1. Downloads a single data point if no dates are specified
+  }
+
+  @Override
+  void appendExchange(final StringBuilder url, final Exchanges exchange) {
+    // e.g. &countrycode=US
+    url.append(COUNTRY_CODE);
+    super.appendExchange(url, exchange);
+  }
+
+  @Override
+  void appendStartDate(final StringBuilder url, final Calendar start) {
+    // &startDate=01-18-1970
+    url.append(START_DATE).append(urlDateFormat.format(start.getTime()));
+    logger.debug("Start date: {}", url);
+  }
+
+  @Override
+  void appendEndDate(final StringBuilder url, final Calendar end) {
+    // &endDate=05-14-2014
+    url.append(END_DATE).append(urlDateFormat.format(end.getTime()));
+    logger.debug("End date: {}", url);
+  }
+
+  @Override
+  void appendDefaultDates(final StringBuilder url,
+                          final Calendar start,
+                          final Calendar end) {
+    appendStartDate(url, DEFAULT_START);
+    appendEndDate(url, Calendar.getInstance());
+    logger.debug("Default start and end dates appended: {}", url);
+  }
+
+  @Override
+  void appendFrequency(final StringBuilder url, final Frequencies frequency) {
+    if ((frequency != null) && (frequency != Frequencies.DAILY)) {
+      url.append(FREQUENCY).append(Character.toUpperCase(frequency.frequency()));
+      logger.debug("Frequency: {}", url);
+    }
+  }
+
+  @Override
+  public TextTransformer newTransformer(final TextTransform transform) {
+    return new TextTransformer(transform) {
+
+      private static final String DATA_START_TAG  = "<DataSeries>";
+      private static final String DATA_END_TAG    = "</DataSeries>";
+      private static final String ERROR_START_TAG = "<ErrorResponse>";
+
+      @Override
+      public List<String> transform(final List<String> lines) {
+        final List<String> newLines = new ArrayList<>(lines.size() >> THREE);
+
+        final StringBuilder newLine = new StringBuilder();
+        for (final String line : lines) {
+          if (line.contains(DATA_START_TAG)) {
+            newLine.delete(ZERO, newLine.length());
+          }
+          else if (line.contains(DATA_END_TAG)) {
+            newLines.add(transform.transform(newLine.toString()));
+          }
+          else if (line.contains(ERROR_START_TAG)) {
+            logger.warn("Erroneous source URL");
+            break;
+          }
+          else {
+            newLine.append(line);
+          }
+        }
+
+        lines.clear();
+        lines.addAll(newLines);
+        logger.info("Transformation complete");
+
+        return lines;
+      }
+
+    };
+  }
+
+  enum Elements {
+    OPEN, HIGH, LOW, CLOSE, VOLUME, DATE;
+
+    private final String name;
+    private final int    offset;
+
+    Elements() {
+      name = name().toLowerCase();
+      offset = name.length() + ONE;
+    }
+
+    @Override
+    public String toString() {
+      return name;
+    }
+
+    public int offset() {
+      return offset;
+    }
+  }
+
+  @Override
+  public TextTransform newTransform(final String symbol) {
+    return new TextTransform() {
+      @Override
+      public String transform(final String line) {
+        // WallStreetJournal XML format
+        //    <id>5</id>    <open>8.85</open>    <high>8.85</high>    <low>8.63</low>    <close>8.69</close>    <volume>8550651</volume>    <date>06/11/2014</date>    <time>2014-06-11 00:00:00.246 EDT</time>    <isDividend>false</isDividend>
+
+        // MetaStock CSV format
+        // Symbol,YYYYMMDD,Open,High,Low,Close,Volume
+        logger.trace(line);
+
+        // indices
+        final int os = line.indexOf(OPEN.toString())                               + OPEN.offset();
+        final int oe = line.indexOf(LESS_THAN, os);
+        final int hs = line.indexOf(HIGH.toString(),   oe + OPEN.offset()   + TWO) + HIGH.offset();
+        final int he = line.indexOf(LESS_THAN, hs);
+        final int ls = line.indexOf(LOW.toString(),    he + HIGH.offset()   + TWO) + LOW.offset();
+        final int le = line.indexOf(LESS_THAN, ls);
+        final int cs = line.indexOf(CLOSE.toString(),  le + LOW.offset()    + TWO) + CLOSE.offset();
+        final int ce = line.indexOf(LESS_THAN, cs);
+        final int vs = line.indexOf(VOLUME.toString(), ce + CLOSE.offset()  + TWO) + VOLUME.offset();
+        final int ve = line.indexOf(LESS_THAN, vs);
+        final int ds = line.indexOf(DATE.toString(),   ve + VOLUME.offset() + TWO) + DATE.offset();
+        final int de = line.indexOf(LESS_THAN, ds);
+
+        // concatenate segments
+        final StringBuilder builder = new StringBuilder(symbol).append(COMMA);
+        final String date = line.substring(ds, de);
+        builder.append(date.substring(SIX))         // year
+               .append(date.substring(ZERO, TWO))   // month
+               .append(date.substring(THREE, FIVE)) // date
+               .append(COMMA);
+        builder.append(line.substring(os, oe))      // open
+               .append(COMMA);
+        builder.append(line.substring(hs, he))      // high
+               .append(COMMA);
+        builder.append(line.substring(ls, le))      // low
+               .append(COMMA);
+        builder.append(line.substring(cs, ce))      // close
+               .append(COMMA);
+        builder.append(line.substring(vs, ve));     // volume
+
+        return builder.toString();
+      }
+    };
+  }
+
+}
