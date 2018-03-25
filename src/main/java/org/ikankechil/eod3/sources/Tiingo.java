e875c7fdbf6a417c3843900e8a0545c627b4b3be
dkcm <daniel.kuan@gmail.com>
2018-03-25 23:58:53 +0800

    Added Tiingo CSV API.

--- src/main/java/org/ikankechil/eod3/sources/Tiingo.java
+++ src/main/java/org/ikankechil/eod3/sources/Tiingo.java
@@ -1,12 +1,13 @@
 /**
- * Tiingo.java  v0.1  15 July 2017 9:59:53 pm
+ * Tiingo.java  v0.2  15 July 2017 9:59:53 pm
  *
- * Copyright © 2017 Daniel Kuan.  All rights reserved.
+ * Copyright © 2017-2018 Daniel Kuan.  All rights reserved.
  */
 package org.ikankechil.eod3.sources;
 
 import static org.ikankechil.eod3.sources.Exchanges.*;
-import static org.ikankechil.eod3.sources.Tiingo.Elements.*;
+import static org.ikankechil.eod3.sources.Tiingo.Formats.*;
+import static org.ikankechil.eod3.sources.Tiingo.JsonElements.*;
 import static org.ikankechil.util.StringUtility.*;
 
 import java.text.DateFormat;
@@ -27,10 +28,11 @@ import org.ikankechil.io.TextTransformer;
  *
  *
  * @author Daniel Kuan
- * @version 0.1
+ * @version 0.2
  */
 public class Tiingo extends Source {
 
+  private final Formats       format;
   private final String        authenticationToken;
   private final DateFormat    urlDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
 
@@ -40,22 +42,25 @@ public class Tiingo extends Source {
   // Date-related URL parameters
   private static final String START_DATE    = "?startDate=";
   private static final String END_DATE      = "&endDate=";
+  private static final String FREQUENCY     = "&resampleFreq=";
 
   private static final String PRICES        = "/prices";
+  private static final String FORMAT        = "&format=";
   private static final String TOKEN         = "&token=";
 
   public Tiingo() {
-    this(null);
+    this(CSV, null);
   }
 
-  public Tiingo(final String authenticationToken) {
+  public Tiingo(final Formats format, final String authenticationToken) {
     super(Tiingo.class);
 
+    this.format = (format == null) ? CSV : format;
     this.authenticationToken = (authenticationToken == null || authenticationToken.isEmpty()) ?
                                AUTH_TOKEN :
                                authenticationToken;
 
-    // supported markets (see https://api.tiingo.com/docs/tiingo/overview)
+    // supported markets (see https://api.tiingo.com/docs/tiingo/overview and https://apimedia.tiingo.com/docs/tiingo/daily/supported_tickers.zip)
     for (final Exchanges exchange : new Exchanges[] { NYSE, NASDAQ, AMEX, ARCA, SSE, SZSE }) {
       exchanges.put(exchange, EMPTY);
     }
@@ -81,51 +86,33 @@ public class Tiingo extends Source {
 
   @Override
   void appendFrequency(final StringBuilder url, final Frequencies frequency) {
-    // do nothing
+    url.append(FREQUENCY).append((frequency != null) ? frequency
+                                                     : DEFAULT_FREQUENCY); // default to daily
   }
 
   @Override
   void appendSuffix(final StringBuilder url) {
+    appendFormat(url);
+    appendAuthenticationToken(url);
+  }
+
+  private void appendFormat(final StringBuilder url) {
+    url.append(FORMAT).append(format.toString().toLowerCase());
+  }
+
+  private void appendAuthenticationToken(final StringBuilder url) {
     if (authenticationToken != null && !authenticationToken.isEmpty()) {
       url.append(TOKEN).append(authenticationToken);
     }
   }
 
-  @Override
-  public TextTransformer newTransformer(final TextTransform transform) {
-    return new TextTransformer(transform) {
-
-      @Override
-      public List<String> transform(final List<String> lines) {
-        final List<String> newLines = new ArrayList<>();
-
-        for (final String line : lines) {
-          for (int i = ZERO; i < line.length(); ++i) {
-            final int start = findNth(OPEN_BRACE, line, ONE, i) + ONE;
-            if (start >= i) {
-              final int end = findNth(CLOSE_BRACE, line, ONE, i = start);
-              final String substring = line.substring(start, i = end);
-              newLines.add(transform.transform(substring));
-            }
-          }
-        }
-
-        lines.clear();
-        lines.addAll(newLines);
-        Collections.reverse(lines);
-
-        return lines;
-      }
-    };
-  }
-
-  enum Elements {
+  enum JsonElements {
     DATE(3), OPEN(2), HIGH(2), LOW(2), CLOSE(2), VOLUME(2);
 
     private final String name;
     private final int    offset;
 
-    Elements(final int offset) {
+    JsonElements(final int offset) {
       name = name().toLowerCase();
       this.offset = name.length() + offset;
     }
@@ -140,56 +127,155 @@ public class Tiingo extends Source {
     }
   }
 
-  @Override
-  public TextTransform newTransform(final String symbol) {
-    return new TextTransform() {
+  public enum Formats {
+    JSON {
       @Override
-      public String transform(final String line) {
-        // Tiingo JSON format (after pre-processing)
-        // {"date":"1980-03-17T00:00:00.000Z","close":62.5,"high":63.5,"low":62.5,"open":62.5,"volume":56900,"adjClose":0.2190423356,"adjHigh":0.2225470129,"adjLow":0.2190423356,"adjOpen":0.2190423356,"adjVolume":199,"divCash":0.0,"splitFactor":1.0},
-        // {"date":"1980-03-18T00:00:00.000Z","close":62.0,"high":63.0,"low":62.0,"open":62.5,"volume":88900,"adjClose":0.2172899969,"adjHigh":0.2207946743,"adjLow":0.2172899969,"adjOpen":0.2190423356,"adjVolume":311,"divCash":0.0,"splitFactor":1.0},
-        // {"date":"1980-03-19T00:00:00.000Z","close":63.5,"high":64.5,"low":63.5,"open":63.5,"volume":96400,"adjClose":0.2225470129,"adjHigh":0.2260516903,"adjLow":0.2225470129,"adjOpen":0.2225470129,"adjVolume":337,"divCash":0.0,"splitFactor":1.0}
+      public TextTransformer newTransformer(final TextTransform transform) {
+        return new TextTransformer(transform) {
+          @Override
+          public List<String> transform(final List<String> lines) {
+            final List<String> newLines = new ArrayList<>();
 
-        // MetaStock CSV format
-        // Symbol,YYYYMMDD,Open,High,Low,Close,Volume
-        // INTC,19800319,63.5,64.5,63.5,63.5,96400
-        // INTC,19800318,62.5,63.0,62.0,62.0,88900
-        // INTC,19800317,62.5,63.5,62.5,62.5,56900
+            for (final String line : lines) {
+              for (int i = ZERO; i < line.length(); ++i) {
+                final int start = findNth(OPEN_BRACE, line, ONE, i) + ONE;
+                if (start >= i) {
+                  final int end = findNth(CLOSE_BRACE, line, ONE, i = start);
+                  final String substring = line.substring(start, i = end);
+                  newLines.add(transform.transform(substring));
+                }
+              }
+            }
 
-        // indices
-        final int ds = line.indexOf(DATE.toString())       + DATE.offset();
-        final int de = line.indexOf(COMMA, ds);
-        final int cs = line.indexOf(CLOSE.toString(),  de) + CLOSE.offset();
-        final int ce = line.indexOf(COMMA, cs);
-        final int hs = line.indexOf(HIGH.toString(),   ce) + HIGH.offset();
-        final int he = line.indexOf(COMMA, hs);
-        final int ls = line.indexOf(LOW.toString(),    he) + LOW.offset();
-        final int le = line.indexOf(COMMA, ls);
-        final int os = line.indexOf(OPEN.toString(),   le) + OPEN.offset();
-        final int oe = line.indexOf(COMMA, os);
-        final int vs = line.indexOf(VOLUME.toString(), oe) + VOLUME.offset();
-        final int ve = line.indexOf(COMMA, vs);
+            lines.clear();
+            lines.addAll(newLines);
+            Collections.reverse(lines);
 
-        // concatenate segments
-        final StringBuilder builder = new StringBuilder(symbol).append(COMMA);
-        final String date = line.substring(ds, de);
-        builder.append(date.substring(ZERO, FOUR))  // year
-               .append(date.substring(FIVE, SEVEN)) // month
-               .append(date.substring(EIGHT, TEN))  // date
-               .append(COMMA);
-        builder.append(line.substring(os, oe))      // open
-               .append(COMMA);
-        builder.append(line.substring(hs, he))      // high
-               .append(COMMA);
-        builder.append(line.substring(ls, le))      // low
-               .append(COMMA);
-        builder.append(line.substring(cs, ce))      // close
-               .append(COMMA);
-        builder.append(line.substring(vs, ve));     // volume
+            return lines;
+          }
+        };
+      }
 
-        return builder.toString();
+      @Override
+      public TextTransform newTransform(final String symbol) {
+        return new TextTransform() {
+          @Override
+          public String transform(final String line) {
+            // Tiingo JSON format (after pre-processing)
+            // {"date":"1980-03-17T00:00:00.000Z","close":62.5,"high":63.5,"low":62.5,"open":62.5,"volume":56900,"adjClose":0.2190423356,"adjHigh":0.2225470129,"adjLow":0.2190423356,"adjOpen":0.2190423356,"adjVolume":199,"divCash":0.0,"splitFactor":1.0},
+            // {"date":"1980-03-18T00:00:00.000Z","close":62.0,"high":63.0,"low":62.0,"open":62.5,"volume":88900,"adjClose":0.2172899969,"adjHigh":0.2207946743,"adjLow":0.2172899969,"adjOpen":0.2190423356,"adjVolume":311,"divCash":0.0,"splitFactor":1.0},
+            // {"date":"1980-03-19T00:00:00.000Z","close":63.5,"high":64.5,"low":63.5,"open":63.5,"volume":96400,"adjClose":0.2225470129,"adjHigh":0.2260516903,"adjLow":0.2225470129,"adjOpen":0.2225470129,"adjVolume":337,"divCash":0.0,"splitFactor":1.0}
+
+            // MetaStock CSV format
+            // Symbol,YYYYMMDD,Open,High,Low,Close,Volume
+            // INTC,19800319,63.5,64.5,63.5,63.5,96400
+            // INTC,19800318,62.5,63.0,62.0,62.0,88900
+            // INTC,19800317,62.5,63.5,62.5,62.5,56900
+
+            // indices
+            final int ds = line.indexOf(DATE.toString())       + DATE.offset();
+            final int de = line.indexOf(COMMA, ds);
+            final int cs = line.indexOf(CLOSE.toString(),  de) + CLOSE.offset();
+            final int ce = line.indexOf(COMMA, cs);
+            final int hs = line.indexOf(HIGH.toString(),   ce) + HIGH.offset();
+            final int he = line.indexOf(COMMA, hs);
+            final int ls = line.indexOf(LOW.toString(),    he) + LOW.offset();
+            final int le = line.indexOf(COMMA, ls);
+            final int os = line.indexOf(OPEN.toString(),   le) + OPEN.offset();
+            final int oe = line.indexOf(COMMA, os);
+            final int vs = line.indexOf(VOLUME.toString(), oe) + VOLUME.offset();
+            final int ve = line.indexOf(COMMA, vs);
+
+            // concatenate segments
+            final StringBuilder builder = new StringBuilder(symbol).append(COMMA);
+            final String date = line.substring(ds, de);
+            builder.append(date.substring(ZERO, FOUR))  // year
+                   .append(date.substring(FIVE, SEVEN)) // month
+                   .append(date.substring(EIGHT, TEN))  // date
+                   .append(COMMA);
+            builder.append(line.substring(os, oe))      // open
+                   .append(COMMA);
+            builder.append(line.substring(hs, he))      // high
+                   .append(COMMA);
+            builder.append(line.substring(ls, le))      // low
+                   .append(COMMA);
+            builder.append(line.substring(cs, ce))      // close
+                   .append(COMMA);
+            builder.append(line.substring(vs, ve));     // volume
+
+            return builder.toString();
+          }
+        };
+      }
+    },
+    CSV {
+      @Override
+      public TextTransformer newTransformer(final TextTransform transform) {
+        return new TextTransformer(transform, ONE, true);
+      }
+
+      @Override
+      public TextTransform newTransform(final String symbol) {
+        return new TextTransform() {
+          @Override
+          public String transform(final String line) {
+            // Tiingo CSV format
+            // date,close,high,low,open,volume,adjClose,adjHigh,adjLow,adjOpen,adjVolume,divCash,splitFactor
+            // 2012-01-03,665.41,668.15,652.37,652.94,7345600,333.735208618,335.109450771,327.195019681,327.480902173,7345600,0.0,1.0
+            // 2012-01-04,668.28,670.25,660.62,665.03,5722200,335.174652042,336.162702057,331.332792589,333.54462029,5722200,0.0,1.0
+            // 2012-01-05,659.01,663.97,656.23,662.13,6559200,330.525299937,333.012979164,329.130995854,332.090130419,6559200,0.0,1.0
+            // 2012-01-06,650.02,660.0,649.79,659.15,5380400,326.016381337,331.021832686,325.901025244,330.595516689,5380400,0.0,1.0
+            // 2012-01-09,622.46,647.0,621.23,646.5,11633500,312.193712081,324.501705679,311.576807757,324.250931563,11633500,0.0,1.0
+
+            // MetaStock CSV format
+            // Symbol,YYYYMMDD,Open,High,Low,Close,Volume
+
+            // locate indices
+            final int closePosition = ELEVEN;
+            final int highPosition = findNth(COMMA, line, ONE, closePosition) + ONE;
+            final int openPosition = findNth(COMMA, line, TWO, highPosition) + ONE;
+            final int volumePosition = findNth(COMMA, line, ONE, openPosition) + ONE;
+            final int adjClosePosition = findNth(COMMA, line, ONE, volumePosition);
+
+            final char[] characters = new char[symbol.length() + adjClosePosition - ONE];
+            // set row name
+            int i = getChars(symbol, ZERO, symbol.length(), characters, ZERO);
+            characters[i] = COMMA;
+            // copy year
+            i = getChars(line, ZERO, FOUR, characters, ++i);
+            // copy month
+            i = getChars(line, FIVE, SEVEN, characters, i);
+            // copy date
+            i = getChars(line, EIGHT, closePosition, characters, i);
+            // copy open
+            i = getChars(line, openPosition, volumePosition, characters, i);
+            // copy high and low
+            i = getChars(line, highPosition, openPosition, characters, i);
+            // copy close
+            i = getChars(line, closePosition, highPosition, characters, i);
+            // copy volume
+            line.getChars(volumePosition, adjClosePosition, characters, i);
+
+            return String.valueOf(characters);
+          }
+        };
       }
     };
+
+    public abstract TextTransformer newTransformer(final TextTransform transform);
+
+    public abstract TextTransform newTransform(final String symbol);
+
+  }
+
+  @Override
+  public TextTransformer newTransformer(final TextTransform transform) {
+    return format.newTransformer(transform);
+  }
+
+  @Override
+  public TextTransform newTransform(final String symbol) {
+    return format.newTransform(symbol);
   }
 
 }
--- src/test/java/org/ikankechil/eod3/sources/TiingoTest.java
+++ src/test/java/org/ikankechil/eod3/sources/TiingoTest.java
@@ -1,5 +1,5 @@
 /**
- * TiingoTest.java  v0.2  15 July 2017 10:37:11 pm
+ * TiingoTest.java  v0.3  15 July 2017 10:37:11 pm
  *
  * Copyright © 2017-2018 Daniel Kuan.  All rights reserved.
  */
@@ -7,7 +7,6 @@ package org.ikankechil.eod3.sources;
 
 import static org.ikankechil.eod3.sources.Exchanges.*;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
@@ -25,7 +24,7 @@ import org.junit.Test;
  *
  *
  * @author Daniel Kuan
- * @version 0.2
+ * @version 0.3
  */
 public class TiingoTest extends SourceTest {
 
@@ -33,10 +32,12 @@ public class TiingoTest extends SourceTest {
   private static final String PRICES        = "/prices";
   private static final String START_DATE    = "?startDate=";
   private static final String END_DATE      = "&endDate=";
+  private static final String FREQUENCY     = "&resampleFreq=";
+  private static final String FORMAT        = "&format=csv";
 
   private final DateFormat    urlDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
 
-  public TiingoTest() throws IOException {
+  public TiingoTest() {
     exchanges.put(NYSE, EMPTY);
     exchanges.put(NASDAQ, EMPTY);
     exchanges.put(AMEX, EMPTY);
@@ -44,16 +45,26 @@ public class TiingoTest extends SourceTest {
     exchanges.put(SSE, EMPTY);
     exchanges.put(SZSE, EMPTY);
 
-    readOriginalLinesFromJsonFile();
+//    readOriginalLinesFromJsonFile();
+    originalLines.addAll(Arrays.asList("date,close,high,low,open,volume,adjClose,adjHigh,adjLow,adjOpen,adjVolume,divCash,splitFactor",
+                                       "2017-12-01,44.68,44.84,43.53,44.73,26640551,44.383516921,44.542455209,43.2411479761,44.433185136,26640551,0.0,1.0",
+                                       "2017-12-04,44.49,45.3,44.33,45.02,26829460,44.194777704,44.999402787,44.0358394161,44.721260783,26829460,0.0,1.0",
+                                       "2017-12-05,43.44,44.9,43.23,44.6,30112131,43.1517451891,44.602057067,42.9431386861,44.304047777,30112131,0.0,1.0",
+                                       "2017-12-06,43.45,43.72,42.67,43.14,24497550,43.1616788321,43.4298871931,42.3868546782,42.8537358991,24497550,0.0,1.0",
+                                       "2017-12-07,43.08,43.6,42.78,43.46,31193561,42.7941340411,43.3106834771,42.4961247512,43.1716124751,31193561,0.0,1.0"));
 
-    transformedLines.addAll(Arrays.asList("INTC,19800319,63.5,64.5,63.5,63.5,96400",
-                                          "INTC,19800318,62.5,63.0,62.0,62.0,88900",
-                                          "INTC,19800317,62.5,63.5,62.5,62.5,56900"));
+    transformedLines.addAll(Arrays.asList("INTC,20171207,43.46,43.6,42.78,43.08,31193561",
+                                          "INTC,20171206,43.14,43.72,42.67,43.45,24497550",
+                                          "INTC,20171205,44.6,44.9,43.23,43.44,30112131",
+                                          "INTC,20171204,45.02,45.3,44.33,44.49,26829460",
+                                          "INTC,20171201,44.73,44.84,43.53,44.68,26640551"));
   }
 
   @Override
   protected URL expectedURL(final String symbol) throws MalformedURLException {
-    return new URL(BASE + symbol + PRICES);
+    return new URL(BASE + symbol + PRICES +
+                   FREQUENCY + Frequencies.DAILY +
+                   FORMAT);
   }
 
   @Override
@@ -65,9 +76,7 @@ public class TiingoTest extends SourceTest {
   @Override
   protected URL expectedURL(final String symbol, final Calendar start, final Calendar end)
       throws MalformedURLException {
-    return new URL(BASE + symbol + PRICES +
-                   START_DATE + urlDateFormat.format(start.getTime()) +
-                   END_DATE + urlDateFormat.format(end.getTime()));
+    return expectedURL(symbol, start, end, null);
   }
 
   @Override
@@ -76,7 +85,7 @@ public class TiingoTest extends SourceTest {
                             final Calendar end,
                             final Frequencies frequency)
       throws MalformedURLException {
-    return expectedURL(symbol, start, end);
+    return expectedURL(symbol, null, start, end, frequency);
   }
 
   @Override
@@ -85,7 +94,7 @@ public class TiingoTest extends SourceTest {
                             final Calendar start,
                             final Calendar end)
       throws MalformedURLException {
-    return expectedURL(symbol, start, end);
+    return expectedURL(symbol, exchange, start, end, null);
   }
 
   @Override
@@ -95,7 +104,11 @@ public class TiingoTest extends SourceTest {
                             final Calendar end,
                             final Frequencies frequency)
       throws MalformedURLException {
-    return expectedURL(symbol, start, end);
+    return new URL(BASE + symbol + PRICES +
+                   START_DATE + urlDateFormat.format(start.getTime()) +
+                   END_DATE + urlDateFormat.format(end.getTime()) +
+                   FREQUENCY + ((frequency != null) ? frequency : Frequencies.DAILY) +
+                   FORMAT);
   }
 
   @Ignore
